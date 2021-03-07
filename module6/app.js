676b7d1be205db7cd5650fc31ebf3cf5d2be51af
mfmoon2 <mike.moon7@gmail.com>
2021-03-07 11:16:58 -0500

    finish assignment 6

--- module6/app.js
+++ module6/app.js
@@ -1,7 +1,55 @@
 (function () {
     'use strict';
+    let successMessage = "Enjoy!";
+    let errorMessage = "Too much!";
+    let emptyMessage = "Please enter data first";
     angular.module('LunchCheck', [])
-        .controller('LunchCheckController', function ($scope) {
-
-        });
+        .controller('LunchCheckController', LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+        $scope.lunchInput = '';
+        $scope.messageReady = false;
+        $scope.emptyDetected = false;
+        $scope.inputClass = "form-group";
+        $scope.messageClass = "form-group message";
+        $scope.validateInput = function () {
+            $scope.messageReady = true;
+            $scope.emptyDetected = false;
+            if ($scope.lunchInput.trim() == '') {
+                $scope.message = emptyMessage;
+                setClasses(false);
+                return;
+            }
+            let inputs = $scope.lunchInput.split(",");
+            let count = 0;
+            for (let i in inputs) {
+                let input = inputs[i].trim();
+                if (input == '' || input.length == 0) {
+                    $scope.emptyDetected = true;
+                    continue;
+                } else {
+                    count++;
+                }
+            }
+            if (count == 0) {
+                $scope.message = emptyMessage;
+                setClasses(false);
+            } else if (count <= 3) {
+                $scope.message = successMessage;
+                setClasses(true);
+            } else {
+                $scope.message = errorMessage;
+                setClasses(true);
+            }
+        }
+        const setClasses = function (isGreen) {
+            if (isGreen) {
+                $scope.messageClass = "form-group message fontGreen";
+                $scope.inputClass = "form-group outlineGreen";
+            } else {
+                $scope.messageClass = "form-group message fontRed";
+                $scope.inputClass = "form-group outlineRed";
+            }
+        };
+    }
 })();
